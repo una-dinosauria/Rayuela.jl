b2d3943a264fe140824d74cf923511c76e9c5c50
una-dinosauria <julm@cs.ubc.ca>
2018-01-13 14:38:10 -0500

    Moving quantization error functions out of utils

---
+++ src/qerrors.jl
@@ -0,0 +1,90 @@
+
+# === Functions that compute quantization error ===
+
+"Reconstruct an encoding"
+function reconstruct(
+  B::Matrix{T1},
+  C::Vector{Matrix{T2}}) where {T1<:Integer, T2<:AbstractFloat}
+
+  m, n = size( B )
+  d, _ = size( C[1] )
+  CB   = zeros( T2, d, n )
+
+  @inbounds for i = 1:m
+    mcb = C[i] # Pick the mth codebook
+    for j = 1:n
+      codej = B[i, j] # Choose the code
+      @simd for k = 1:d     # And copy it all
+        CB[k, j] += mcb[k, codej]
+      end
+    end
+  end
+
+  return CB
+end
+
+"Compute cost of encoding in each vector"
+function veccost(
+  X::Matrix{T1},
+  B::Matrix{T2},
+  C::Vector{Matrix{T1}}) where {T1 <: AbstractFloat, T2 <: Integer}
+
+  # This implementation is devectorized and minimizes extra memory allocated
+
+  d, n = size( X )
+  m,_ = size(B)
+
+  cost = zeros(T, n)
+  CB   = zeros(T, d)
+
+  @inbounds for i = 1:n # Loop over vectors in X
+    for k = 1:m # Loop over codebooks
+      Ci = C[k]
+      code = B[k, i] # Choose the code
+      @simd for j = 1:d # add the codebook entry
+        CB[j] += Ci[j, code]
+      end
+    end
+
+    # Compute the squared error
+    @simd for j = 1:d
+      cost[i] += (CB[j] - X[j,i]).^2
+      CB[j] = 0.0
+    end
+  end
+
+  cost
+end
+
+"Get the total cost of encoding"
+function qerror(
+  X::Matrix{T1},
+  B::Matrix{T2},
+  C::Vector{Matrix{T1}}) where {T1 <: AbstractFloat, T2 <: Integer}
+  mean(veccost(X,B,C))
+end
+
+"Get the total error of an OPQ encoding"
+function qerror_opq(
+  X::Matrix{T1},
+  B::Matrix{T2},
+  C::Vector{Matrix{T1}},
+  R::Matrix{T1}) where {T1 <: AbstractFloat, T2 <: Integer}
+
+  CB = similar( X )
+  subdims = splitarray( 1:size(X,1), length(C) )
+  for i=1:length(C)
+    CB[subdims[i], :] = C[i][:, vec(B[i,:]) ]
+  end
+
+  mean(sum((R*CB - X).^2, 1 ))
+end
+
+"Get the total error of a PQ encoding"
+function qerror_pq(
+  X::Matrix{T1},
+  B::Matrix{T2},
+  C::Vector{Matrix{T1}}) where {T1 <: AbstractFloat, T2 <: Integer}
+  R = eye(T1, size(X,1))
+  qerror_opq(X, B, C, R)
+end
--- src/utils.jl
+++ src/utils.jl
@@ -4,19 +4,20 @@ export qerror, qerror_pq, qerror_opq, quantize_norms, splitarray,# sparsify_code
 
 "Quantize the norms of an encoding"
 function quantize_norms(
-  B::Matrix{T1},         # Codes
-  C::Vector{Matrix{T2}}, # Codebooks
-  cbnorms::Vector{T2}) where {T1<:Integer, T2<:AbstractFloat} # codebook for norms
+  B::Matrix{T1},         # m-by-n. Codes
+  C::Vector{Matrix{T2}}, # m-long h-by-d each. Codebooks
+  cbnorms::Vector{T2}) where {T1<:Integer, T2<:AbstractFloat} # h-long. norms codebooks
 
   CB = reconstruct(B,C)
   d, n = size( CB )
   _, h = size( C[1] )
 
-  dbnormsB = Vector{T1}(n)
+  dbnormsB   = Vector{T1}(n)
   dists2norm = Vector{T2}(h)
 
   @inbounds for i = 1:n
-    ithnorm::T2 = 0.0
+    ithnorm::T2 = zero(T2)
+
     @simd for j = 1:d
       ithnorm += CB[j,i].^2
     end
@@ -24,6 +25,8 @@ function quantize_norms(
     for j = 1:h
       dists2norm[j] = (ithnorm - cbnorms[j]).^2
     end
+
+    # Store of the index of the minimum distance
     _, dbnormsB[i] = findmin( dists2norm )
   end
 
@@ -174,137 +177,4 @@ function splitarray(
   end
 
   return out
-end
-
-# Split a matrix horizontally in equal parts
-# if the number of x is a multiple of nparts, then each parts has the same number of
-# elements.
-# if that's not the case, then the n rest elements, e.t. the rest of the division, will be
-# added to the first n parts. So the first n parts contain each 1 item more than the the
-# parts from n + 1 to nparts
-function splitMatrixh{F <: AbstractFloat}(
-  x::Matrix{F}, # Vector to split in equal parts
-  nparts::Integer)             # Number of parts to split the vector on
-
-  out = Vector{Matrix{F}}(nparts)
-  _,n = size(x)
-  outidx = splitarray(1:n,nparts)
-  for i = 1:nparts
-    out[i] = x[:,outidx[i]]
-  end
-
-  return out, outidx
-end
-
-###############################################
-## Functions that evaluate global objectives ##
-###############################################
-
-function reconstruct(
-  B::Matrix{T1},
-  C::Vector{Matrix{T2}}) where {T1<:Integer, T2<:AbstractFloat}
-
-  m, n = size( B )
-  d, _ = size( C[1] )
-  CB   = zeros( T2, d, n )
-
-  @inbounds for i = 1:m
-    mcb = C[i] # Pick the mth codebook
-    for j = 1:n
-      codej = B[i, j] # Choose the code
-      @simd for k = 1:d     # And copy it all
-        CB[k, j] += mcb[k, codej]
-      end
-    end
-  end
-
-  return CB
-end
-
-"Compute cost of encoding in each vector"
-function veccost(
-  X::Matrix{T},
-  B::Matrix{Int16},
-  C::Vector{Matrix{T}}) where {T <: AbstractFloat}
-
-  d, n = size( X )
-  m,_ = size(B)
-
-  cost = zeros(T, n)
-  CBi  = zeros(T, d)
-
-  @inbounds for i = 1:n
-
-    for k = 1:m
-      Ci = C[k]
-      code = B[k, i] # Choose the code
-      @simd for j = 1:d     # And copy it all
-        CBi[j] += Ci[j, code]
-      end
-    end
-
-    @simd for j = 1:d
-      cost[i] += (CBi[j] - X[j,i]).^2
-      CBi[j] = 0.0
-    end
-  end
-
-  return cost
-
-end
-
-# Evaluates the objective function for full-dimensional codebooks
-function qerror{T <: AbstractFloat}(
-  X::Matrix{T},
-  B::Matrix{Int16},
-  C::Vector{Matrix{T}})
-
-  d, n = size( X )
-  m,_ = size(B)
-
-  accum::T = 0
-  CBi = zeros(T,d)
-
-  @inbounds for i = 1:n
-
-    for k = 1:m
-      Ci = C[k]
-      code = B[k, i] # Choose the code
-      @simd for j = 1:d     # And copy it all
-        CBi[j] += Ci[j, code]
-      end
-    end
-
-    @simd for j = 1:d
-      accum += (CBi[j] - X[j,i]).^2
-      CBi[j] = 0
-    end
-  end
-
-  return accum ./ n
-end
-
-# Evaluates the objective functions for non full-dimensional C
-function qerror_pq{T1 <: AbstractFloat, T2 <: Integer}(
-  X::Matrix{T1},
-  B::Matrix{T2},
-  C::Vector{Matrix{T1}})
-
-  d, n = size( X )
-  return qerror_opq( X, B, C, eye(T1, d) )
-end
-
-function qerror_opq{T1 <: AbstractFloat, T2 <: Integer}(
-  X::Matrix{T1},
-  B::Matrix{T2},
-  C::Vector{Matrix{T1}},
-  R::Matrix{T1})
-
-  CB = similar( X )
-  subdims = splitarray( 1:size(X,1), length(C) )
-  for i=1:length(C)
-    CB[ subdims[i], : ] = C[i][:, vec(B[i,:]) ]
-  end
-
-  return mean(sum((R*CB - X).^2, 1 ))
 end
