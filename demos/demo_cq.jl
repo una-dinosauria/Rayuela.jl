ff4a7ef2108a88ac11698b78fe854ef2463c6433
Julieta Martinez <julieta@uber.com>
2018-03-26 21:49:20 -0400

    Preprocessing code and more wrapper code for CQ

--- demos/demo_cq.jl
+++ demos/demo_cq.jl
@@ -48,7 +48,8 @@ function test_experiment()
   rec = eval_recall(gt, idx, knn)
 end
 
-function run_experiments()
+
+function run_experiments(dataset_name)
 
   ntrials = 10
   for trial = 1:ntrials
@@ -58,6 +59,39 @@ function run_experiments()
     a = CQ_parameters()
     a.output_file_prefix = "/home/julieta/Desktop/CQ/build/results/sift/trial_$trial/"
     # a.max_iter=1
+
+    if dataset_name == "SIFT1M"
+      # do nothing
+    elseif dataset_name == "MNIST"
+      a.points_count     = Int(60e3)
+      a.space_dimension  = 28 * 28
+      a.mu               = 0.00001f0
+      a.queries_count    = Int(10e3)
+      a.groundtruth_length = 1
+      a.points_file      = "/home/julieta/Desktop/CQ/build/data/mnist/mnist_learn.fvecs"
+      a.queries_file     = "/home/julieta/Desktop/CQ/build/data/mnist/mnist_query.fvecs"
+      a.groundtruth_file = "/home/julieta/Desktop/CQ/build/data/mnist/mnist_groundtruth.ivecs"
+    elseif dataset_name == "labelme"
+      a.points_count     = 20019
+      a.space_dimension  = 512
+      a.mu               = 100f0
+      a.queries_count    = 2000
+      a.groundtruth_length = 1
+      a.points_file      = "/home/julieta/Desktop/CQ/build/data/labelme/labelme_learn.fvecs"
+      a.queries_file     = "/home/julieta/Desktop/CQ/build/data/labelme/labelme_query.fvecs"
+      a.groundtruth_file = "/home/julieta/Desktop/CQ/build/data/labelme/labelme_groundtruth.ivecs"
+    # elseif
+      # a.points_count     = Int(1e5)
+      # a.space_dimension  = 128
+      # a.mu               = 100f0
+      # a.queries_count    = 2000
+      # a.groundtruth_length = 1
+      # a.points_file      = "/home/julieta/Desktop/CQ/build/data/labelme/labelme_learn.fvecs"
+      # a.queries_file     = "/home/julieta/Desktop/CQ/build/data/labelme/labelme_query.fvecs"
+      # a.groundtruth_file = "/home/julieta/Desktop/CQ/build/data/labelme/labelme_groundtruth.ivecs"
+    else
+      error("dataset unknown")
+    end
 
     # Prepare output folder
     if !Base.Filesystem.ispath(a.output_file_prefix)
@@ -75,5 +109,38 @@ function run_experiments()
   end
 end
 
-@show("run_experiments")
-run_experiments()
+function dump_dataset(dataset_name, verbose=true)
+  if dataset_name == "MNIST"
+    nquery, nbase, ntrain = Int(10e3), Int(60e3), Int(60e3)
+  elseif dataset_name == "labelme"
+    nquery, nbase, ntrain = Int(2e3), Int(20019), Int(20019)
+  elseif dataset_name == "Convnet1M"
+    nquery, nbase, ntrain = Int(1e4), Int(1e6), Int(1e5)
+  else
+    error("dataset unknown")
+  end
+
+  Xt = Rayuela.read_dataset(dataset_name * "_base", ntrain)
+  Xb = Rayuela.read_dataset(dataset_name * "_base", nbase)
+  Xq = Rayuela.read_dataset(dataset_name * "_query", nquery, verbose)[:,1:nquery]
+  gt = Rayuela.read_dataset(dataset_name * "_groundtruth", nquery, verbose)
+
+  bpath = joinpath("/home/julieta/Desktop/CQ/build/data/", lowercase(dataset_name))
+  if !Base.Filesystem.ispath(bpath)
+    mkpath(bpath)
+  end
+
+  fvecs_write(Xt, joinpath(bpath, "$(lowercase(dataset_name))_learn.fvecs"))
+  fvecs_write(Xb, joinpath(bpath, "$(lowercase(dataset_name))_base.fvecs"))
+  fvecs_write(Xq, joinpath(bpath, "$(lowercase(dataset_name))_query.fvecs"))
+  ivecs_write(convert(Matrix{Int32},gt), joinpath(bpath, "$(lowercase(dataset_name))_groundtruth.ivecs"))
+end
+
+# dump_dataset("MNIST")
+# dump_dataset("labelme")
+# dump_dataset("Convnet1M")
+
+# @show("run_experiments")
+run_experiments("MNIST")
+run_experiments("labelme")
+# run_experiments("Convnet1M")
