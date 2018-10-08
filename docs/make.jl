fd5e997a73b67ca7940ec6a5c3d8a1fa985ae59b
una-dinosauria <julieta@uber.com>
2018-10-08 17:06:37 -0400

    Push so docs can be generated

--- docs/make.jl
+++ docs/make.jl
@@ -15,3 +15,12 @@ makedocs(
     ]
     # doctest = test
 )
+
+deploydocs(
+	repo = "github.com/una-dinosauria/Rayuela.jl.git",
+	target = "build",
+	osname = "linux",
+	julia  = "1.0",
+	deps = nothing,
+	make = nothing,
+)
